8bec6a07b4ad8d528ba4d2e5241c470bf7ad91af
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2021-09-11 12:48:50 -0300

    [linked-lists] reversing using stack

--- linkedlist/reverse.go
+++ linkedlist/reverse.go
@@ -17,3 +17,39 @@ func Reverse(p *Node) *Node {
 	reverse(&head, p)
 	return head
 }
+
+// TODO: Remove from here and add to stack
+func top(stack []*Node) *Node {
+	return stack[len(stack)-1]
+}
+func pop(stack []*Node) []*Node {
+	return stack[:len(stack)-1]
+}
+
+func ReverseUsingStack(head *Node) *Node {
+	if head == nil {
+		return head
+	}
+
+	var stack []*Node
+	temp := head
+
+	for temp != nil {
+		stack = append(stack, temp)
+		temp = temp.Next
+	}
+
+	temp = top(stack)
+	head = temp
+
+	stack = pop(stack)
+
+	for len(stack) > 0 {
+		temp.Next = top(stack)
+		stack = pop(stack)
+		temp = temp.Next
+	}
+	temp.Next = nil
+
+	return head
+}
